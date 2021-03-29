56b5132b3df1d943aa333a19d8d58d9ffd5fd6db
sob-user <oulad.b.s@gmail.com>
2021-03-29 10:16:09 +0200

    save project

---
+++ e2ee.js
@@ -0,0 +1,44 @@
+const crypto = require('crypto');
+const aes256 = require('aes256');
+
+function createKey() {
+    let ECDH = crypto.createECDH('secp256k1');
+    ECDH.generateKeys();
+    return ECDH;
+}
+
+function publicKeys(key) {
+    return key.getPublicKey().toString('base64');
+}
+
+function sharedKey(key, publicKey) {
+    return key.computeSecret(publicKey, 'base64', 'hex');
+}
+
+function checkSharedKeys(firstKey, secondKey) {
+    if(firstKey === secondKey) {
+        console.log('shared key generated with success');
+    } else {
+        console.log('something went wrong while you trying to generate shared key')
+    }
+}
+
+const firstPeerKey = createKey();
+const secondPeerKey = createKey();
+
+const firstPeerPublicKey =  publicKeys(firstPeerKey);
+const secondPeerPublicKey = publicKeys(secondPeerKey);
+
+const firstPeerSharedKey = sharedKey(firstPeerKey, secondPeerPublicKey);
+const secondPeerSharedKey = sharedKey(secondPeerKey, firstPeerPublicKey);
+
+checkSharedKeys(firstPeerSharedKey, secondPeerSharedKey);
+
+const firstPeerMessage = "Hi! I'm sending you an encrypted message";
+const encryptedMessage = aes256.encrypt(firstPeerSharedKey, firstPeerMessage);
+
+const secondPeerMessageReceived = encryptedMessage;
+console.log('message send: ',secondPeerMessageReceived);
+
+const secondDecryptedMessage = aes256.decrypt(secondPeerSharedKey, secondPeerMessageReceived);
+console.log('message received decrypted: ', secondDecryptedMessage);
